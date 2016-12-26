6a487c3d1e5708054ae3647b51da7c69c1d2141a
Dmitry Khramov <dmitrii.hramov@mail.ru>
2016-12-26 18:31:31 +0200

    Test graph added

--- Mobile/sensorreader/src/pages/home/home.ts
+++ Mobile/sensorreader/src/pages/home/home.ts
@@ -12,13 +12,41 @@ export class HomePage {
 
     sensors: any = [];
 
-    constructor(public nav: NavController, public dataService: Data, public platform: Platform) {}
-
-    ionViewDidLoad(){
-
+    constructor(public nav: NavController, public dataService: Data, public platform: Platform) {
         this.platform.ready().then(() => {
-            this.sensors = this.dataService.getLastValues();
-            console.log(this.sensors);
+            this.dataService.getLastValues().subscribe(data => {
+
+                let rowNum = 0;
+
+                    for (let i = 0; i < data.length; i+=2) {
+                        this.sensors[rowNum] = Array(2);
+
+                        if (data[i]) {
+
+                            this.sensors[rowNum][0] = {
+                                name: data[i].name,
+                                value: data[i].last_value.value,
+                                url: data[i].values_url,
+                                timestamp: data[i].last_value.timestamp
+                            }
+                        }
+
+                        if (data[i+1]) {
+                            this.sensors[rowNum][1] = {
+                                name: data[i+1].name,
+                                value: data[i+1].last_value.value,
+                                url: data[i+1].values_url,
+                                timestamp: data[i+1].last_value.timestamp
+                            }
+                        }
+
+                        rowNum++;
+                    }
+
+                }, (err) => {
+                    console.log(err);
+            });
+
         });
     }
 
@@ -27,10 +55,10 @@ export class HomePage {
         var date = new Date(timestampEnd);
         date.setDate(date.getDate() - 1);
         let timestampStart = date.getTime();
-
-        let values = this.dataService.getSensorValues(sensorUrl, timestampStart, timestampEnd);
         this.nav.push(SensorDetailPage, {
-            values: values
+            url: sensorUrl,
+            start: timestampStart,
+            end: timestampEnd
         });
     }
 
--- Mobile/sensorreader/src/pages/sensor-detail/sensor-detail.ts
+++ Mobile/sensorreader/src/pages/sensor-detail/sensor-detail.ts
@@ -1,21 +1,97 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
+import {Data} from "../../providers/data";
+
+declare var FusionCharts;
 
 @Component({
     selector: 'page-sensor-detail',
     templateUrl: 'sensor-detail.html'
 })
+
 export class SensorDetailPage {
 
-    sensor: any;
+    url: any;
+    start: any;
+    end: any;
 
-    constructor(public nav: NavController, public params: NavParams) {
+    constructor(public nav: NavController, public params: NavParams, public dataService: Data) {
 
-        this.sensor = this.params.get('values');
+        this.url = this.params.get('url');
+        this.start = this.params.get('start');
+        this.end = this.params.get('end');
+
+        this.dataService.getSensorValues(this.url, this.start, this.end).subscribe(data => {
+
+            let graphData = this.createArrayOfLabelValues(data);
+            console.log(graphData);
+            this.drawTheChart(graphData);
+
+        }, (err) => {
+            console.log(err);
+        });
     }
 
-    ionViewDidLoad() {
-        // console.log(this.sensor);
+
+    drawTheChart(values: any) {
+
+        FusionCharts.ready(function() {
+            var sensorChart = new FusionCharts({
+                type: 'area2d',
+                renderAt: 'chart-container',
+                width: '100%',
+                height: '50%',
+                dataFormat: 'json',
+                dataSource: {
+                    "chart": {
+                        "caption": "Sensor's statistics",
+                        "subCaption": "Last day",
+                        "xAxisName": "Time",
+                        "yAxisName": "Value",
+                        "paletteColors": "#0075c2",
+                        "bgColor": "#ffffff",
+                        "showBorder": "0",
+                        "showCanvasBorder": "0",
+                        "plotBorderAlpha": "10",
+                        "usePlotGradientColor": "0",
+                        "plotFillAlpha": "50",
+                        "showXAxisLine": "1",
+                        "axisLineAlpha": "25",
+                        "divLineAlpha": "10",
+                        "showValues": "1",
+                        "showAlternateHGridColor": "0",
+                        "captionFontSize": "14",
+                        "subcaptionFontSize": "14",
+                        "subcaptionFontBold": "0",
+                        "toolTipColor": "#ffffff",
+                        "toolTipBorderThickness": "0",
+                        "toolTipBgColor": "#000000",
+                        "toolTipBgAlpha": "80",
+                        "toolTipBorderRadius": "2",
+                        "toolTipPadding": "5"
+                    },
+
+                    "data": values
+                }
+            }).render();
+        });
+
     }
+
+    createArrayOfLabelValues(values: any) {
+
+        let dataArray = [];
+
+        for(let i = values.length - 1; i >= 0; i-- ) {
+            dataArray.push({
+                "label": new Date(values[i].timestamp).getHours().toString(),
+                "value": values[i].value
+            });
+        }
+
+        return dataArray;
+    }
+
+
 
 }
